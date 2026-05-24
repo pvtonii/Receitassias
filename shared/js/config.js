9fbbc862076ee6f73fed4ca13c349484a8977a5b
pvtonii <pvtoni@hotmail.com>
2026-05-24 02:40:41 -0400

    Update app version to 1.8.1

--- shared/js/config.js
+++ shared/js/config.js
@@ -6,7 +6,7 @@
    suba o numero abaixo. Os dois footers leem daqui.
 ============================================ */
 
-const APP_VERSION = "1.8.0";
+const APP_VERSION = "1.8.1";
 const APP_DATA    = "2026-05-24";
 
 /* ============================================
